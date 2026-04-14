1c80164aedb32bb2a41a54e6bdc225f1e34c3d8c
Zexolver <Zexolver@Disroot.org>
2026-04-14 09:32:34 -0400

    Fix socket buffer handling and enhance broadcasting
    Updated the _listen method to drain the socket buffer properly and added aggressive broadcast strategy in the broadcast method.

--- networking.py
+++ networking.py
@@ -6,17 +6,18 @@ import time
 class NetworkManager:
     def __init__(self, player_id, port=5005):
         self.player_id = player_id
-        self.peers = {} # Format: {id: {x, y, last_seen}}
+        self.peers = {} 
         self.port = port
         self.running = True
         
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        self.sock.setblocking(False)
+        
+        # This makes the socket throw an error if we try to read an empty buffer
+        self.sock.setblocking(False) 
         
         try:
-            # Bind to all interfaces
             self.sock.bind(('', self.port))
         except Exception as e:
             print(f"Network Bind Error: {e}")
@@ -26,24 +27,44 @@ class NetworkManager:
 
     def _listen(self):
         while self.running:
-            try:
-                data, addr = self.sock.recvfrom(2048)
-                msg = json.loads(data.decode())
-                if msg['id'] != self.player_id:
-                    msg['last_seen'] = time.time()
-                    self.peers[msg['id']] = msg
-            except:
-                pass
+            # --- THE FIX: DRAIN THE BUFFER ---
+            while True:
+                try:
+                    data, addr = self.sock.recvfrom(2048)
+                    msg = json.loads(data.decode())
+                    if msg['id'] != self.player_id:
+                        msg['last_seen'] = time.time()
+                        self.peers[msg['id']] = msg
+                except:
+                    # When the buffer is completely empty, it throws an error.
+                    # We catch it and break out of the reading loop.
+                    break
             
-            # Clean up timed-out peers (older than 3 seconds)
+            # Clean up old peers
             now = time.time()
             self.peers = {pid: p for pid, p in self.peers.items() if now - p['last_seen'] < 3}
             time.sleep(0.01)
 
     def broadcast(self, state):
         state['id'] = self.player_id
+        msg = json.dumps(state).encode()
+        
+        # Aggressive Broadcast Strategy
+        targets = [
+            "<broadcast>",        
+            "255.255.255.255",    
+        ]
+        
         try:
-            # Send to the universal broadcast address
-            self.sock.sendto(json.dumps(state).encode(), ("255.255.255.255", self.port))
+            local_ip = socket.gethostbyname(socket.gethostname())
+            subnet_broadcast = local_ip.rsplit('.', 1)[0] + '.255'
+            if subnet_broadcast not in targets:
+                targets.append(subnet_broadcast)
         except:
             pass
+
+        for target in targets:
+            try:
+                self.sock.sendto(msg, (target, self.port))
+            except:
+                pass
